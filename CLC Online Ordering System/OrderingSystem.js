40d14b255226380ba1606b63f074af85997ee260
Justin Waugh <6343830+jwaugh@users.noreply.github.com>
2020-07-12 17:05:14 -0400

    OrderingSystem.js creation

---
+++ CLC Online Ordering System/OrderingSystem.js
@@ -0,0 +1,18 @@
+function OrderingSystem() {
+  function RestaurantPicker() {
+  }
+  function RestaurantMenuDisplay() {
+  }
+  function OrderTaker() {
+  }
+  function ConfirmOrder() {
+  }
+  function PaymentMethod() {
+  }
+  function ProcessPayment() {
+  }
+  function VerifyPaymennt() {
+  }
+  function IssueReceipt() {
+  }
+}
