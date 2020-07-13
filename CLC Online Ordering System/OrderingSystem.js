de32e75aed6aaab85fda28ae30496b221374bef0
Justin Waugh <6343830+jwaugh@users.noreply.github.com>
2020-07-12 21:42:06 -0400

    test

--- CLC Online Ordering System/OrderingSystem.js
+++ CLC Online Ordering System/OrderingSystem.js
@@ -17,8 +17,9 @@ function OrderingSystem() {
     console.log("RestaurantMenuDisplay() called")
     document.write("<div class=\"row\"><div class=\"column\"></div><div class=\"column\"></div></div>");
     // Make sure a notes section is available to user to add special instructions to cook, delivery driver, etc.
+    OrderingSystem.RestaurantMenuDisplay = RestaurantMenuDisplay
   }
-  
+  RestaurantMenuDisplay();
   
   // Show order to verify correct. Use a large "Complete Order" button at bottom to finish ordering process
   function ConfirmOrder() {
@@ -46,9 +47,9 @@ function OrderingSystem() {
   }
 
   OrderingSystem.ConfirmOrder = ConfirmOrder;
-}
-
+} 
 OrderingSystem();
+
 
 function myFunction() {
     // Get the checkbox
